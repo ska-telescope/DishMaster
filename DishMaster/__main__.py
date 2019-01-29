3ed637ddd4c8a85d27c2aed219f2433e76e7f33f
vinodsathe <vinodnsathe.ska@gmail.com>
2019-01-29 20:15:00 +0530

    AT1-167: Create DishMaster project for GitLab CI testing.

---
+++ DishMaster/__main__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the DishMaster project
+#
+#
+#
+# Distributed under the terms of the BSD-3-Clause license.
+# See LICENSE.txt for more info.
+
+from __future__ import absolute_import
+from .DishMaster import main
+main()
